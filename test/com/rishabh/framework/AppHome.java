0688fb483a4679dfa9adb28bb6f181f160b45d98
Rishabh Jaiswal <rshbh.jaiswal@gmail.com>
2021-05-18 01:37:00 +0530

    Custom Based Automation Framework- II

---
+++ test/com/rishabh/framework/AppHome.java
@@ -0,0 +1,20 @@
+package com.rishabh.framework;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppHome {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+
+	@Test
+	public void TestMethod() {
+		System.out.println("Hello Jaiswal!");
+	}
+
+}
